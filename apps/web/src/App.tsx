cc8300aa0ac6fc20d721b43600606868e6801f2f
arun_murali0 <arunkarthick0810@gmail.com>
2026-08-14 17:44:33 +0530

    project-started...

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@repo/ui/components";
+
+function App() {
+  return (
+    <div className="bg-accent">
+      <div className="text-red-500">Home</div>
+      <Button>click</Button>
+    </div>
+  );
+}
+
+export default App;
